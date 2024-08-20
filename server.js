97e51d02a8acbbb35c052662ef1a4ff89ed2c7d1
Bilalnapollo <bilal.kashif@napollo.online>
2024-08-20 11:08:32 -0700

    changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 // const user = os.userInfo()
 
